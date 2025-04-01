8a4a6bef5c62c6db8aa8efc7ac6ef8117b902529
Khaing Min Htwe <kmhtwe1999@gmail.com>
2025-03-31 23:27:41 -0700

    modify user get 5

--- app/api-doc/react-swagger.tsx
+++ app/api-doc/react-swagger.tsx
@@ -4,10 +4,15 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 type Props = {
-  spec: Record<string, unknown>; // more specific than `any`
+  spec: Record<string, unknown>; // Accept any object with string keys
 };
 
 function ReactSwagger({ spec }: Props) {
+  // Make sure spec is valid (in case you want to add extra validation or fallback)
+  if (!spec || typeof spec !== 'object') {
+    return <div>Error: Swagger specification is invalid or missing.</div>;
+  }
+
   return <SwaggerUI spec={spec} />;
 }
 
